a2c70ac9d7055ec5076541b18d6d93702ce1946b
MATHEUS PEREIRA DE AGUIAR <25204557@dsn127m1249389.cfp127aluno.local>
2026-09-21 08:36:25 -0300

    Adicionando comentários

--- app/src/main/java/com/example/componentesestado/MainActivity.kt
+++ app/src/main/java/com/example/componentesestado/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.componentesestado
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,10 +12,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -22,11 +26,13 @@ import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.componentesestado.ui.theme.ComponentesEstadoTheme
 import com.example.componentesestado.ui.theme.Hunin
+import kotlin.math.log
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,10 +52,20 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun BasicComponentsScreen(modifier: Modifier = Modifier) {
+    // Guarda o valor digitado no campo de texto
+    val textFieldValue = remember {
+        // Cria uma variavel mutável iniciando vazia
+        mutableStateOf("")
+    }
+
+    // Organiza os componentes um abaixo do outro
     Column(modifier = modifier
+        // Ocupa todo o espaço disponivel
         .fillMaxSize()
+        // Define a cor de fundo da tela
         .background(Color(239, 247, 207))
     ){
+        // Exibe um texto na tela
         Text(text = "Aulas Android",
             fontSize = 32.sp,
             fontWeight = FontWeight.Bold,
@@ -58,27 +74,45 @@ fun BasicComponentsScreen(modifier: Modifier = Modifier) {
             modifier = Modifier
                 .background(Color.Magenta)
                 .fillMaxWidth(),
+            // Alinha o texto no final
             textAlign = TextAlign.End,
+            // Define o espacamento entre as letras
             letterSpacing = 4.sp
         )
+        // Exibe o subtitulo na tela
         Text(text = "Com Jetpack Compose",
             fontSize = 16.sp,
             fontWeight = FontWeight.SemiBold,
             color = Color(66, 133, 244),
             fontFamily = Hunin,
             modifier = Modifier
+                // Centraliza o componente horizontalmente
                 .align(Alignment.CenterHorizontally)
         )
 
+        // Cria um campo para entrada de texto
         TextField(
-            value = "Feliz é aquele que nao é triste",
-             onValueChange =  {},
-            modifier = Modifier
-                .fillMaxWidth()
+
+            //Pega o valor atual da variavel que é mutável
+            value = textFieldValue.value,
+            // Atualiza o valor quando o usuario digita, sendo mutável
+            onValueChange =  { novoValor ->
+                // Atribui o novo valor digitado
+                 textFieldValue.value = novoValor
+                //Ve o log em tempo real da mensagem que chega
+                Log.i("teste", novoValor)
+                             },
+            modifier = Modifier.fillMaxWidth(),
+            // Define as configuracoes do teclado
+            keyboardOptions = KeyboardOptions(
+                // Define o teclado numerico
+                keyboardType = KeyboardType.Email
+            )
         )
 
         AndroidEnemy(
             color =    Color.Yellow,
+            // Define o tamanho do inimigo
             modifier = Modifier.size(100.dp)
         )
         AndroidEnemy(
@@ -89,16 +123,21 @@ fun BasicComponentsScreen(modifier: Modifier = Modifier) {
         )
     }
 }
-
+// Cria um componente reutilizavel
 @Composable
 fun AndroidEnemy(
     color: Color,
+    // Recebe as configuracoes do componente
     modifier: Modifier = Modifier
 ) {
+    // Exibe uma imagem na tela
     Image(
         modifier = modifier,
+        // Carrega a imagem dos recursos
         painter = painterResource(R.drawable.ic_launcher_foreground),
+        // Aplica a cor recebida na imagem
         colorFilter = ColorFilter.tint(color = color),
+        // Define a descricao da imagem
         contentDescription = "Android Enemy"
     )
 }
